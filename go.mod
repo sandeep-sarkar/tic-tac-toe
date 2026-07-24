b6e245d9761da41887d0fe4fbaece33e00484643
Sandeep Sarkar <sand2rock@gmail.com>
2026-07-24 22:32:58 +0100

    Implement core board game logic

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sandeep-sarkar/tic-tac-toe
+
+go 1.25.0
---
+++ internal/game/constants.go
@@ -0,0 +1,9 @@
+package game
+
+const (
+	StatusNotStarted = "Not Started"
+	StatusInProgress = "In Progress"
+	StatusWon        = "Won"
+	StatusDraw       = "Draw"
+	StatusReady      = "Ready"
+)
